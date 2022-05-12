9d762c67b58c90f71ec3dbb6d513576f56374ee5
aston314 <aston.he@gmail.com>
2022-05-12 19:50:55 +0800

    Update tv.js

--- tv.js
+++ tv.js
@@ -157,15 +157,7 @@
       {
         title: 'SAMSUNGTV',
         url: 'http://proxy.cub.watch/cdn/https://rentry.co/SAMSUNGTV/raw'
-      },
-      {
-        title: 'VinteraTV',
-        url: 'http://twicker.pp.ua/vintera'
-      },
-      {
-        title: 'KinoBoom',
-        url: 'http://twicker.pp.ua/kinoboom/'
-    }];
+      }];
       Lampa.Listener.follow('app', function (e) {
         if (e.type == 'ready') {var ico = '<svg xmlns="http://www.w3.org/2000/svg"   viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tv"><rect x="2" y="7" width="20" height="15" rx="2" ry="2"></rect><polyline points="17 2 12 7 7 2"></polyline></svg>';
            var menu_item = $('<li class="menu__item selector focus" data-action="rtv"><div class="menu__ico">' + ico + '</div><div class="menu__text">电视</div></li>');
