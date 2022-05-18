857b349bd069037c7f07d70fd9b67e1dc830afdb
aston314 <aston.he@gmail.com>
2022-05-18 09:51:41 +0800

    Update online.js

--- online.js
+++ online.js
@@ -12,7 +12,7 @@
       var results = [];
       var filtred = [];
       var extract = {};
-      var token = '3i40G5TSECmLF77oAqnEgbx61ZWaOYaE';
+      var token = '';
       var last;
       var last_filter;
       var filter_items = {
@@ -823,6 +823,8 @@
                a = Lampa.Storage.field('online_3_url'); 
               };*/
               //console.log(Lampa.Storage.field('online_3_url'));
+
+
               item.file = doreg.prefix_video+unescape(a.replace(/\+/g,  " "));
             };
               //console.log(item.Link);
@@ -879,33 +881,102 @@
               });
               Lampa.Player.playlist(playlist);
             } else {*/
-            if (file) {
-              _this4.start();
+            if (file.indexOf('http') == -1) {
+              function fn1() {
+                  return new Promise(function(resolve, reject) {
+                        // ... some code
+                        cordovaHTTP.get("https://sh-data-s01.chinaeast2.cloudapp.chinacloudapi.cn/xt.php", {
+                        url: file,
+                        next: "",
+                        id: 0,
+                        nid: 1
+                    }, { Referer: "https://www.libvio.com/" }, function(response) {
+                        // prints 200
+                        console.log(response.status);
+                        //Lampa.Noty.show(response.status);
+                        try {
+                            //console.log(response.data);
+                            var mathurl = response.data.replace(/\n|\r/g, '').match(new RegExp("var urls = '(.+?)'", 'g'));
+                            var file = mathurl.toString().replace("var urls = '","").replace("'","");
+                            //console.log(urlbody);
+                            //Lampa.Storage.set('online_3_url', urlbody);
+                            if (file) {
+                            _this4.start();
 
-              var playlist = [];
-              var first = {
-                url: file,
-                timeline: view,
-                title: element.season ? element.title : object.movie.title + ' / ' + element.title+ ' / ' + element.quality
-              };
-              Lampa.Player.play(first);
-              //object.movie.number_of_seasons!== 'undefined' element.season
-              if (element.season) {
-                items.forEach(function (elem) {
-                  console.log(elem)
-                  console.log("elem")
-                  playlist.push({
-                    title: elem.quality+ ' / ' +elem.title,
-                    url: _this4.getFile(elem)
+                            var playlist = [];
+                            var first = {
+                              url: file,
+                              timeline: view,
+                              title: element.season ? element.title : object.movie.title + ' / ' + element.title+ ' / ' + element.quality
+                            };
+                            Lampa.Player.play(first);
+                            //object.movie.number_of_seasons!== 'undefined' element.season
+                            if (element.season) {
+                              items.forEach(function (elem) {
+                                console.log(elem)
+                                console.log("elem")
+                                playlist.push({
+                                  title: elem.quality+ ' / ' +elem.title,
+                                  url: _this4.getFile(elem)
+                                });
+                              });
+                            } else {
+                              playlist.push(first);
+                            }
+
+                            Lampa.Player.playlist(playlist);
+                          } else {
+                            Lampa.Noty.show('无法检索链接');
+                          }
+                            resolve(value)
+                        } catch(e) {
+                            console.error("parsing error");
+                            //Lampa.Noty.show("播放地址解析出错");
+                        }
+                    }, function(response) {
+                        // prints 403
+                        reject(error);
+                        console.log(response.status);
+                        
+                        //prints Permission denied 
+                        console.log(response.error);
+                    });
+
                   });
-                });
-              } else {
-                playlist.push(first);
               }
+              fn1().then(function(value){
+                  //do something
+              })
+            }
+            else{
+              if (file) {
+                _this4.start();
 
-              Lampa.Player.playlist(playlist);
-            } else {
-              Lampa.Noty.show('无法检索链接');
+                var playlist = [];
+                var first = {
+                  url: file,
+                  timeline: view,
+                  title: element.season ? element.title : object.movie.title + ' / ' + element.title+ ' / ' + element.quality
+                };
+                Lampa.Player.play(first);
+                //object.movie.number_of_seasons!== 'undefined' element.season
+                if (element.season) {
+                  items.forEach(function (elem) {
+                    console.log(elem)
+                    console.log("elem")
+                    playlist.push({
+                      title: elem.quality+ ' / ' +elem.title,
+                      url: _this4.getFile(elem)
+                    });
+                  });
+                } else {
+                  playlist.push(first);
+                }
+
+                Lampa.Player.playlist(playlist);
+              } else {
+                Lampa.Noty.show('无法检索链接');
+              }
             }
           });
           scroll.append(item);
@@ -963,7 +1034,7 @@
     function startPlugin() {
       window.plugin_online1_ready = true;
       Lampa.Component.add('online1', component);
-      Lampa.Template.add('button_online1', "<div class=\"full-start__button selector view--online\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" version=\"1.1\" width=\"512\" height=\"512\" x=\"0\" y=\"0\" viewBox=\"0 0 30.051 30.051\" style=\"enable-background:new 0 0 512 512\" xml:space=\"preserve\" class=\"\">\n    <g xmlns=\"http://www.w3.org/2000/svg\">\n        <path d=\"M19.982,14.438l-6.24-4.536c-0.229-0.166-0.533-0.191-0.784-0.062c-0.253,0.128-0.411,0.388-0.411,0.669v9.069   c0,0.284,0.158,0.543,0.411,0.671c0.107,0.054,0.224,0.081,0.342,0.081c0.154,0,0.31-0.049,0.442-0.146l6.24-4.532   c0.197-0.145,0.312-0.369,0.312-0.607C20.295,14.803,20.177,14.58,19.982,14.438z\" fill=\"currentColor\"/>\n        <path d=\"M15.026,0.002C6.726,0.002,0,6.728,0,15.028c0,8.297,6.726,15.021,15.026,15.021c8.298,0,15.025-6.725,15.025-15.021   C30.052,6.728,23.324,0.002,15.026,0.002z M15.026,27.542c-6.912,0-12.516-5.601-12.516-12.514c0-6.91,5.604-12.518,12.516-12.518   c6.911,0,12.514,5.607,12.514,12.518C27.541,21.941,21.937,27.542,15.026,27.542z\" fill=\"currentColor\"/>\n    </g></svg>\n\n    <span>在线观看</span>\n    </div>");
+      Lampa.Template.add('button_online1', "<div class=\"full-start__button selector view--online\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" version=\"1.1\" width=\"512\" height=\"512\" x=\"0\" y=\"0\" viewBox=\"0 0 30.051 30.051\" style=\"enable-background:new 0 0 512 512\" xml:space=\"preserve\" class=\"\">\n    <g xmlns=\"http://www.w3.org/2000/svg\">\n        <path d=\"M19.982,14.438l-6.24-4.536c-0.229-0.166-0.533-0.191-0.784-0.062c-0.253,0.128-0.411,0.388-0.411,0.669v9.069   c0,0.284,0.158,0.543,0.411,0.671c0.107,0.054,0.224,0.081,0.342,0.081c0.154,0,0.31-0.049,0.442-0.146l6.24-4.532   c0.197-0.145,0.312-0.369,0.312-0.607C20.295,14.803,20.177,14.58,19.982,14.438z\" fill=\"currentColor\"/>\n        <path d=\"M15.026,0.002C6.726,0.002,0,6.728,0,15.028c0,8.297,6.726,15.021,15.026,15.021c8.298,0,15.025-6.725,15.025-15.021   C30.052,6.728,23.324,0.002,15.026,0.002z M15.026,27.542c-6.912,0-12.516-5.601-12.516-12.514c0-6.91,5.604-12.518,12.516-12.518   c6.911,0,12.514,5.607,12.514,12.518C27.541,21.941,21.937,27.542,15.026,27.542z\" fill=\"currentColor\"/>\n    </g></svg>\n\n    <span>观看</span>\n    </div>");
       Lampa.Template.add('online1', "<div class=\"online selector\">\n        <div class=\"online__body\">\n            <div class=\"online__title\">{title}</div>\n            <div class=\"online__quality\">七霞电影 / {quality}</div>\n        </div>\n    </div>");
       Lampa.Listener.follow('full', function (e) {
         if (e.type == 'complite') {
