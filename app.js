454bc0f6b898694453abc44b2f8dcd0f87f2fb64
aston314 <aston.he@gmail.com>
2022-10-26 13:20:29 +0800

    Auto-update-2022-10-26T13:20:27.388+08:00

--- app.js
+++ app.js
@@ -1876,6 +1876,7 @@
         cordovaFetch.setTimeout = 60;
         cordovaFetch(url, para_fetch)
             .then(function (response) {
+              console.log(response)
                 if (dataType == 'json') {
                     return response.json();
                 } else if (dataType == 'text') {
