446c37f12b11d567a429ff244b291fa8b72abea1
aston314 <aston.he@gmail.com>
2022-11-17 19:48:28 +0800

    Update alist.js

--- www/build/plugins/alist.js
+++ www/build/plugins/alist.js
@@ -763,7 +763,7 @@
         Lampa.Settings.main().update()
       };
       var ico = '<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-triangle"><path d="M10.29 3.86 1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"/></svg>';
-      var menu_item = $('<li class="menu__item selector focus" data-action="mypikpak"><div class="menu__ico">' + ico + '</div><div class="menu__text">Alist</div></li>');
+      var menu_item = $('<li class="menu__item selector focus" data-action="alist"><div class="menu__ico">' + ico + '</div><div class="menu__text">Alist</div></li>');
       var element = {};
       element.img = './img/img_broken.svg';
       element.original_title = '';
