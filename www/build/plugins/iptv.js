01d69e2e274b1c5bb01176a143b80e79c0a6579e
aston314 <aston.he@gmail.com>
2022-11-17 22:35:45 +0800

    Update iptv.js

--- www/build/plugins/iptv.js
+++ www/build/plugins/iptv.js
@@ -1,13 +1,13 @@
 /*
 // https://ss-iptv.com/ru/operators/catchup
 // niklabs.com/catchup-settings/
-// http://example.com/iptv/00000000000000/9201/index.m3u8?utc=1666796400&lutc=1666826200
+// http://plwxk8hl.russtv.net/iptv/00000000000000/9201/index.m3u8?utc=1666796400&lutc=1666826200
 */
 ;(function () {
     'use strict';
     var plugin = {
         component: 'my_iptv',
-        icon: "<svg height=\"244\" viewBox=\"0 0 260 244\" xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\"><path d=\"M259.5 47.5v114c-1.709 14.556-9.375 24.723-23 30.5a2934.377 2934.377 0 0 1-107 1.5c-35.704.15-71.37-.35-107-1.5-13.625-5.777-21.291-15.944-23-30.5v-115c1.943-15.785 10.61-25.951 26-30.5a10815.71 10815.71 0 0 1 208 0c15.857 4.68 24.523 15.18 26 31.5zm-230-13a4963.403 4963.403 0 0 0 199 0c5.628 1.128 9.128 4.462 10.5 10 .667 40 .667 80 0 120-1.285 5.618-4.785 8.785-10.5 9.5-66 .667-132 .667-198 0-5.715-.715-9.215-3.882-10.5-9.5-.667-40-.667-80 0-120 1.35-5.18 4.517-8.514 9.5-10z\" fill=\"white\"/><path d=\"M70.5 71.5c17.07-.457 34.07.043 51 1.5 5.44 5.442 5.107 10.442-1 15-5.991.5-11.991.666-18 .5.167 14.337 0 28.671-.5 43-3.013 5.035-7.18 6.202-12.5 3.5a11.529 11.529 0 0 1-3.5-4.5 882.407 882.407 0 0 1-.5-42c-5.676.166-11.343 0-17-.5-4.569-2.541-6.069-6.375-4.5-11.5 1.805-2.326 3.972-3.992 6.5-5zM137.5 73.5c4.409-.882 7.909.452 10.5 4a321.009 321.009 0 0 0 16 30 322.123 322.123 0 0 0 16-30c2.602-3.712 6.102-4.879 10.5-3.5 5.148 3.334 6.314 7.834 3.5 13.5a1306.032 1306.032 0 0 0-22 43c-5.381 6.652-10.715 6.652-16 0a1424.647 1424.647 0 0 0-23-45c-1.691-5.369-.191-9.369 4.5-12zM57.5 207.5h144c7.788 2.242 10.288 7.242 7.5 15a11.532 11.532 0 0 1-4.5 3.5c-50 .667-100 .667-150 0-6.163-3.463-7.496-8.297-4-14.5 2.025-2.064 4.358-3.398 7-4z\" fill=\"white\"/></svg>",
+        icon: "<svg height=\"244\" viewBox=\"0 0 260 244\" xmlns=\"http://www.w3.org/2000/svg\" style=\"fill-rule:evenodd;\" fill=\"currentColor\"><path d=\"M259.5 47.5v114c-1.709 14.556-9.375 24.723-23 30.5a2934.377 2934.377 0 0 1-107 1.5c-35.704.15-71.37-.35-107-1.5-13.625-5.777-21.291-15.944-23-30.5v-115c1.943-15.785 10.61-25.951 26-30.5a10815.71 10815.71 0 0 1 208 0c15.857 4.68 24.523 15.18 26 31.5zm-230-13a4963.403 4963.403 0 0 0 199 0c5.628 1.128 9.128 4.462 10.5 10 .667 40 .667 80 0 120-1.285 5.618-4.785 8.785-10.5 9.5-66 .667-132 .667-198 0-5.715-.715-9.215-3.882-10.5-9.5-.667-40-.667-80 0-120 1.35-5.18 4.517-8.514 9.5-10z\"/><path d=\"M70.5 71.5c17.07-.457 34.07.043 51 1.5 5.44 5.442 5.107 10.442-1 15-5.991.5-11.991.666-18 .5.167 14.337 0 28.671-.5 43-3.013 5.035-7.18 6.202-12.5 3.5a11.529 11.529 0 0 1-3.5-4.5 882.407 882.407 0 0 1-.5-42c-5.676.166-11.343 0-17-.5-4.569-2.541-6.069-6.375-4.5-11.5 1.805-2.326 3.972-3.992 6.5-5zM137.5 73.5c4.409-.882 7.909.452 10.5 4a321.009 321.009 0 0 0 16 30 322.123 322.123 0 0 0 16-30c2.602-3.712 6.102-4.879 10.5-3.5 5.148 3.334 6.314 7.834 3.5 13.5a1306.032 1306.032 0 0 0-22 43c-5.381 6.652-10.715 6.652-16 0a1424.647 1424.647 0 0 0-23-45c-1.691-5.369-.191-9.369 4.5-12zM57.5 207.5h144c7.788 2.242 10.288 7.242 7.5 15a11.532 11.532 0 0 1-4.5 3.5c-50 .667-100 .667-150 0-6.163-3.463-7.496-8.297-4-14.5 2.025-2.064 4.358-3.398 7-4z\"/></svg>",
         name: 'ipTV'
     };
     var lists = [];
@@ -283,120 +283,127 @@
                 emptyResult();
                 return;
             } else {
-                network.native(
-                    prepareUrl(object.url),
-                    function (data) {
-                        if (typeof data != 'string'
-                            || data.substr(0, 7).toUpperCase() !== "#EXTM3U"
-                        ) {
-                            emptyResult();
-                            return;
-                        }
-                        catalog = {
-                            '': {
-                                title: langGet('favorites'),
-                                channels: []
-                            }
-                        };
-                        lists[object.id].groups = [{
+                var parseList = function (data) {
+                    if (typeof data != 'string'
+                        || data.substr(0, 7).toUpperCase() !== "#EXTM3U"
+                    ) {
+                        emptyResult();
+                        return;
+                    }
+                    catalog = {
+                        '': {
                             title: langGet('favorites'),
-                            key: ''
-                        }];
-                        var l = data.split(/\r?\n/);
-                        var cnt = 0, i = 1, chNum = 0, m, mm, defGroup = defaultGroup;
-                        while (i < l.length) {
-                            chNum = cnt + 1;
-                            var channel = {
-                                ChNum: chNum,
-                                Title: "Ch " + chNum,
-                                isYouTube: false,
-                                Url: '',
-                                Group: '',
-                                Options: {}
-                            };
-                            for (; cnt < chNum && i < l.length; i++) {
-                                if (!!(m = l[i].match(/^#EXTGRP:\s*(.+?)\s*$/i))
-                                    && m[1].trim() !== ''
+                            channels: []
+                        }
+                    };
+                    lists[object.id].groups = [{
+                        title: langGet('favorites'),
+                        key: ''
+                    }];
+                    var l = data.split(/\r?\n/);
+                    var cnt = 0, i = 1, chNum = 0, m, mm, defGroup = defaultGroup;
+                    while (i < l.length) {
+                        chNum = cnt + 1;
+                        var channel = {
+                            ChNum: chNum,
+                            Title: "Ch " + chNum,
+                            isYouTube: false,
+                            Url: '',
+                            Group: '',
+                            Options: {}
+                        };
+                        for (; cnt < chNum && i < l.length; i++) {
+                            if (!!(m = l[i].match(/^#EXTGRP:\s*(.+?)\s*$/i))
+                                && m[1].trim() !== ''
+                            ) {
+                                defGroup = m[1].trim();
+                            } else if (!!(m = l[i].match(/^#EXTINF:\s*-?\d+(\s+\S.*?\s*)?,(.+)$/i))) {
+                                channel.Title = m[2].trim();
+                                if (!!m[1]
+                                    && !!(m = m[1].match(/([^\s=]+)=((["'])(.*?)\3|\S+)/g))
                                 ) {
-                                    defGroup = m[1].trim();
-                                } else if (!!(m = l[i].match(/^#EXTINF:\s*-?\d+(\s+\S.*?\s*)?,(.+)$/i))) {
-                                    channel.Title = m[2].trim();
-                                    if (!!m[1]
-                                        && !!(m = m[1].match(/([^\s=]+)=((["'])(.*?)\3|\S+)/g))
-                                    ) {
-                                        for (var j = 0; j < m.length; j++) {
-                                            if (!!(mm = m[j].match(/([^\s=]+)=((["'])(.*?)\3|\S+)/))) {
-                                                channel[mm[1].toLowerCase()] = mm[4] || mm[2];
-                                            }
+                                    for (var j = 0; j < m.length; j++) {
+                                        if (!!(mm = m[j].match(/([^\s=]+)=((["'])(.*?)\3|\S+)/))) {
+                                            channel[mm[1].toLowerCase()] = mm[4] || mm[2];
                                         }
                                     }
-                                } else if (!!(m = l[i].match(/^#EXTVLCOPT:\s*([^\s=]+)=(.+)$/i))) {
-                                    channel.Options[m[1].trim().toLowerCase()] = m[2].trim();
                                 }
-                                // else if (!!(m = l[i].match(/^(https?|udp|rt[ms]?p|mms|acestream):\/\/(.+)$/i))) {
-                                else if (!!(m = l[i].match(/^(https?):\/\/(.+)$/i))) {
-                                    channel.Url = m[0].trim();
-                                    channel.isYouTube = !!(m[2].match(/^(www\.)?youtube\.com/));
-                                    channel.Group = channel['group-title'] || defGroup;
-                                    cnt++;
-                                }
+                            } else if (!!(m = l[i].match(/^#EXTVLCOPT:\s*([^\s=]+)=(.+)$/i))) {
+                                channel.Options[m[1].trim().toLowerCase()] = m[2].trim();
                             }
-                            if (!!channel.Url && !channel.isYouTube) {
-                                if (!catalog[channel.Group]) {
-                                    catalog[channel.Group] = {
-                                        title: channel.Group,
-                                        channels: []
-                                    };
-                                    lists[object.id].groups.push({
-                                        title: channel.Group,
-                                        key: channel.Group
-                                    });
-                                }
-                                if (!channel['tvg-logo'] && channel['Title'] !== "Ch " + chNum) {
-                                    channel['tvg-logo'] = 'http://epg.rootu.top/picon/'
-                                        + encodeURIComponent(channel['Title']) + '.png';
-                                }
-                                catalog[channel.Group].channels.push(channel);
-                                var favI = favorite.indexOf(favID(channel.Title));
-                                if (favI !== -1) {
-                                    catalog[''].channels[favI] = channel;
-                                }
+                            // else if (!!(m = l[i].match(/^(https?|udp|rt[ms]?p|mms|acestream):\/\/(.+)$/i))) {
+                            else if (!!(m = l[i].match(/^(https?):\/\/(.+)$/i))) {
+                                channel.Url = m[0].trim();
+                                channel.isYouTube = !!(m[2].match(/^(www\.)?youtube\.com/));
+                                channel.Group = channel['group-title'] || defGroup;
+                                cnt++;
                             }
                         }
-                        for (i = 0; i < lists[object.id].groups.length; i++) {
-                            var group = lists[object.id].groups[i];
-                            group.title += ' [' + catalog[group.key].channels.length + ']';
-                        }
-                        for (i = 0; i < favorite.length; i++) {
-                            if (!catalog[''].channels[i]) {
-                                catalog[''].channels[i] = {
-                                    ChNum: -1,
-                                    Title: "#" + favorite[i],
-                                    isYouTube: false,
-                                    Url: 'http://epg.rootu.top/empty/_.m3u8',
-                                    Group: '',
-                                    Options: {},
-                                    'tvg-logo': 'http://epg.rootu.top/empty/_.gif'
+                        if (!!channel.Url && !channel.isYouTube) {
+                            if (!catalog[channel.Group]) {
+                                catalog[channel.Group] = {
+                                    title: channel.Group,
+                                    channels: []
                                 };
+                                lists[object.id].groups.push({
+                                    title: channel.Group,
+                                    key: channel.Group
+                                });
+                            }
+                            if (!channel['tvg-logo'] && channel['Title'] !== "Ch " + chNum) {
+                                channel['tvg-logo'] = 'http://epg.rootu.top/picon/'
+                                    + encodeURIComponent(channel['Title']) + '.png';
+                            }
+                            catalog[channel.Group].channels.push(channel);
+                            var favI = favorite.indexOf(favID(channel.Title));
+                            if (favI !== -1) {
+                                catalog[''].channels[favI] = channel;
                             }
                         }
-                        _this.build(
-                            !catalog[object.currentGroup]
-                                ? (lists[object.id].groups.length > 1 && !!catalog[lists[object.id].groups[1].key]
+                    }
+                    for (i = 0; i < lists[object.id].groups.length; i++) {
+                        var group = lists[object.id].groups[i];
+                        group.title += ' [' + catalog[group.key].channels.length + ']';
+                    }
+                    for (i = 0; i < favorite.length; i++) {
+                        if (!catalog[''].channels[i]) {
+                            catalog[''].channels[i] = {
+                                ChNum: -1,
+                                Title: "#" + favorite[i],
+                                isYouTube: false,
+                                Url: 'http://epg.rootu.top/empty/_.m3u8',
+                                Group: '',
+                                Options: {},
+                                'tvg-logo': 'http://epg.rootu.top/empty/_.gif'
+                            };
+                        }
+                    }
+                    _this.build(
+                        !catalog[object.currentGroup]
+                            ? (lists[object.id].groups.length > 1 && !!catalog[lists[object.id].groups[1].key]
                                     ? catalog[lists[object.id].groups[1].key]['channels']
                                     : []
-                                )
-                                : catalog[object.currentGroup]['channels']
+                            )
+                            : catalog[object.currentGroup]['channels']
+                    );
+                }
+                var listUrl = prepareUrl(object.url);
+                network.native(
+                    listUrl,
+                    parseList,
+                    function () {
+                        // Возможно ошибка из-за CORS пробуем silent запрос через CORS прокси
+                        network.silent(
+                            'https://epg.rootu.top/cors.php?url=' + encodeURIComponent(listUrl)
+                                + '&uid=' + utils.uid() + '&sig=' + generateSigForString(listUrl),
+                            parseList,
+                            emptyResult,
+                            false,
+                            {dataType: 'text'}
                         );
                     },
-                    function () {
-                        // todo попробовать silent запрос через CORS прокси
-                        emptyResult();
-                    },
                     false,
-                    {
-                        dataType: 'text'
-                    }
+                    {dataType: 'text'}
                 )
             }
             return this.render();
@@ -580,9 +587,11 @@
             } else {
                 var empty = new Lampa.Empty();
                 html.append(empty.render());
-                this.start = empty.start;
+                // this.start = empty.start;
                 this.activity.loader(false);
-                this.activity.toggle();
+                // this.activity.toggle();
+                Lampa.Controller.collectionSet(info);
+                Navigator.move('right');
             }
         };
         this.selectGroup = function () {
@@ -624,10 +633,8 @@
                         Navigator.move('up');
                     } else {
                         if (!info.find('.view--category').hasClass('focus')) {
-                            if (!info.find('.view--category').hasClass('focus')) {
-                                Lampa.Controller.collectionSet(info);
-                                Navigator.move('right')
-                            }
+                            Lampa.Controller.collectionSet(info);
+                            Navigator.move('right')
                         } else Lampa.Controller.toggle('head');
                     }
                 },
@@ -748,7 +755,7 @@
     );
     langAdd('settings_list_url_desc1',
         {
-            ru: 'Вы можете добавить еще один плейлист суда. Ссылки на плейлисты обычно заканчиваются на <i>.m3u</i> или <i>.m3u8</i>',
+            ru: 'Вы можете добавить еще один плейлист здесь. Ссылки на плейлисты обычно заканчиваются на <i>.m3u</i> или <i>.m3u8</i>',
             uk: 'Ви можете додати ще один плейлист суду. Посилання на плейлисти зазвичай закінчуються на <i>.m3u</i> або <i>.m3u8</i>',
             be: 'Вы можаце дадаць яшчэ адзін плэйліст суда. Спасылкі на плэйлісты звычайна заканчваюцца на <i>.m3u</i> або <i>.m3u8</i>',
             en: 'You can add another trial playlist. Playlist links usually end with <i>.m3u</i> or <i>.m3u8</i>',
@@ -833,7 +840,7 @@
             uk: 'Зрушити вгору',
             be: 'Ссунуць уверх',
             en: 'Move up',
-            zh: '提升'
+            zh: '上移'
         }
     );
     langAdd('favorites_move_down',
