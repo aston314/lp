ba0bef4b83153e73f3e6ffcc68106eb074a255fa
aston314 <aston.he@gmail.com>
2023-02-16 21:51:22 +0800

    Update onlinenew.js

--- www/build/plugins/onlinenew.js
+++ www/build/plugins/onlinenew.js
@@ -3114,8 +3114,8 @@
       }, {
         dataType: 'text',
         headers: {
-          'Referer': 'https://t-rex.tzfile.com/',
-          'User-Agent': UC_UA
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+          'Referer': 'https://t-rex.tzfile.com/'
         }
       });
     };
