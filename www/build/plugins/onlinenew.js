9e623365944dd21f6129d443bb7937e25216ccf1
aston314 <aston.he@gmail.com>
2023-03-24 10:50:47 +0800

    Update onlinenew.js

--- www/build/plugins/onlinenew.js
+++ www/build/plugins/onlinenew.js
@@ -44,7 +44,7 @@
         search_html_selector: '',
         link_folder: 'detail',
         //detail_url_selector: '.stui-pannel__head:contains(LINE)+ul',
-        detail_url_selector: '.stui-content+.stui-vodlist__head > ul',
+        detail_url_selector: '.stui-pannel__head:contains(BD播放) + ul',
         videoparse: 'default',
         videocontainer: '.MacPlayer',
         use_referer: true,
