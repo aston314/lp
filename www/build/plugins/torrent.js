d6b58a0c177887e75ab44946f127779741810fa1
aston314 <aston.he@gmail.com>
2023-06-28 20:03:39 +0800

    Update torrent.js

--- www/build/plugins/torrent.js
+++ www/build/plugins/torrent.js
@@ -227,7 +227,7 @@
                 // html.append(empty.render());
                 
                 // console.log(a)
-                // Lampa.Noty.show(network.errorDecode(a, c));
+                Lampa.Noty.show(network.errorDecode(a, c));
                 if (a.hasOwnProperty("responseText")) {
                     var empty = new Lampa.Empty({
                         descr: '哦，无法获取 ' + object.title + ' 的内容。'
@@ -697,17 +697,15 @@
         }
 
         var button = "<div class=\"full-start__button selector view--online_findtorrent\" data-subtitle=\"\">\n    <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 50 50\" width=\"50px\" height=\"50px\"> <path d=\"M25,2C12.317,2,2,12.317,2,25s10.317,23,23,23s23-10.317,23-23S37.683,2,25,2z M40.5,30.963c-3.1,0-4.9-2.4-4.9-2.4 S34.1,35,27,35c-1.4,0-3.6-0.837-3.6-0.837l4.17,9.643C26.727,43.92,25.874,44,25,44c-2.157,0-4.222-0.377-6.155-1.039L9.237,16.851 c0,0-0.7-1.2,0.4-1.5c1.1-0.3,5.4-1.2,5.4-1.2s1.475-0.494,1.8,0.5c0.5,1.3,4.063,11.112,4.063,11.112S22.6,29,27.4,29 c4.7,0,5.9-3.437,5.7-3.937c-1.2-3-4.993-11.862-4.993-11.862s-0.6-1.1,0.8-1.4c1.4-0.3,3.8-0.7,3.8-0.7s1.105-0.163,1.6,0.8 c0.738,1.437,5.193,11.262,5.193,11.262s1.1,2.9,3.3,2.9c0.464,0,0.834-0.046,1.152-0.104c-0.082,1.635-0.348,3.221-0.817,4.722 C42.541,30.867,41.756,30.963,40.5,30.963z\" fill=\"currentColor\"></path> </svg>\n\n    <span>搜索磁力</span>\n    </div>";
-        if (Lampa.Storage.get('online_torrentsite_balanser') == '') {
-            Lampa.Storage.set('online_torrentsite_balanser', catalogs[0].Keyword);
-        }
-
-        var balanser = Lampa.Storage.get('online_torrentsite_balanser');
         
-
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
                 var btn = $(button);
                 btn.on('hover:enter', function () {
+                    if (Lampa.Storage.get('online_torrentsite_balanser') == '') {
+                        Lampa.Storage.set('online_torrentsite_balanser', catalogs[0].Keyword);
+                    }
+                    var balanser = Lampa.Storage.get('online_torrentsite_balanser');
                     Lampa.Activity.push({
                         url: '',
                         title: '磁力搜索 - ' + e.data.movie.title,
