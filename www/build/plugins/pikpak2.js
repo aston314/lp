48f1c951d620f6baa2d42cafc9dc7a1f5248237c
aston314 <aston.he@gmail.com>
2023-07-04 20:59:06 +0800

    Update pikpak2.js

--- www/build/plugins/pikpak2.js
+++ www/build/plugins/pikpak2.js
@@ -929,7 +929,10 @@
             // Lampa.Noty.show(network.errorDecode(a, c));
             if (error) error();
         }, getAsUriParameters(postdata), {
-            dataType: 'json'
+            dataType: 'json',
+            headers: {
+              'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+            }
         });
 
     }
