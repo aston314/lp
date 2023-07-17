539b508bb3620e4bf4890c711ec81a2e47b2337d
aston314 <aston.he@gmail.com>
2023-07-17 13:46:10 +0800

    Update db_review.js

--- www/build/plugins/db_review.js
+++ www/build/plugins/db_review.js
@@ -7,39 +7,43 @@
         // Lampa.Controller.toggle('full_start');
         if (kpid != '') {
             $.get('https://movie.douban.com/j/subject_suggest?q=' + imdbid, function (data) { 
-                network["native"]('https://m.douban.com/rexxar/api/v2/movie/' + data[0].id + '/interests?count=30&order_by=hot&anony=0&start=0&ck=&for_mobile=1', function (json) {
-                    var button = json.interests.map(function(element) {
-                      return '<div class="items-line__head" style="margin-bottom: 0.4em;"><div class="items-line__title">' + element.create_time + '</div><div>' + (element.rating ? element.rating.value + '颗星 ':'') +'评论人: ' + element.user.name + '</div></div><div class="items-line__body"><div class="full-descr"><div class="full-descr__left"><div>' + element.comment + '</div></div></div></div>';
-                    }).join('');
-                    
+                if (data.length) {
+                    network["native"]('https://m.douban.com/rexxar/api/v2/movie/' + data[0].id + '/interests?count=30&order_by=hot&anony=0&start=0&ck=&for_mobile=1', function (json) {
+                        var button = json.interests.map(function (element) {
+                            return '<div class="items-line__head" style="margin-bottom: 0.4em;"><div class="items-line__title">' + element.create_time + '</div><div>' + (element.rating ? element.rating.value + '颗星 ' : '') + '评论人: ' + element.user.name + '</div></div><div class="items-line__body"><div class="full-descr"><div class="full-descr__left"><div>' + element.comment + '</div></div></div></div>';
+                        }).join('');
 
-                    var modal = $('<div><div class="broadcast__text" style="text-align:left"><div class="otzyv">' + button + '</div></div></div>');
-                    // var enabled = Lampa.Controller.enabled().name;
-                    Lampa.Modal.open({
-                        title: "",
-                        html: modal,
-                        size: "large",
-                        mask: !0,
-                        onBack: function () {
-                            Lampa.Modal.close(), Lampa.Controller.toggle('full_start')
-                            // Lampa.Controller.toggle(enabled)
-                        },
-                        onSelect: function () { }
+
+                        var modal = $('<div><div class="broadcast__text" style="text-align:left"><div class="otzyv">' + button + '</div></div></div>');
+                        // var enabled = Lampa.Controller.enabled().name;
+                        Lampa.Modal.open({
+                            title: "",
+                            html: modal,
+                            size: "large",
+                            mask: !0,
+                            onBack: function () {
+                                Lampa.Modal.close(), Lampa.Controller.toggle('full_start')
+                                // Lampa.Controller.toggle(enabled)
+                            },
+                            onSelect: function () { }
+                        });
+                    }, function (a, c) {
+                        // console.log(a.responseJSON)
+                        if (a.responseJSON.code == 1287) {
+                            Lampa.Noty.show('只支持在Android客户端上展示。');
+                        } else {
+                            Lampa.Noty.show(network.errorDecode(a, c));
+                        }
+                    }, false, {
+                        dataType: 'json',
+                        headers: {
+                            'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2007J3SC Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045714 Mobile Safari/537.36',
+                            'Referer': "https://m.douban.com/movie/subject/" + data[0].id + "/comments"
+                        }
                     });
-                }, function (a, c) {
-                    // console.log(a.responseJSON)
-                    if (a.responseJSON.code == 1287){
-                        Lampa.Noty.show('只支持在Android客户端上展示。');
-                    } else {
-                        Lampa.Noty.show(network.errorDecode(a, c));
-                    }
-                }, false, {
-                    dataType: 'json',
-                    headers: {
-                        'User-Agent': 'Mozilla/5.0 (Linux; Android 11; M2007J3SC Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045714 Mobile Safari/537.36',
-                        'Referer': "https://m.douban.com/movie/subject/" + data[0].id + "/comments"
-                    }
-                });
+                } else {
+                    Lampa.Noty.show('没有找到影评。');  
+                }
 
             });
 
