3cd818c302c0a8cca71158312e3e9a60989c3e9c
aston314 <aston.he@gmail.com>
2023-07-17 13:00:00 +0800

    Update db_review.js

--- www/build/plugins/db_review.js
+++ www/build/plugins/db_review.js
@@ -8,9 +8,9 @@
             $.get('https://movie.douban.com/j/subject_suggest?q=' + imdbid, function (data) {
                 // console.log(data[0])
                 // 
-                network["native"]('https://m.douban.com/rexxar/api/v2/movie/' + data[0].id + '/interests?count=20&order_by=hot&anony=0&start=0&ck=&for_mobile=1', function (json) {
+                network["native"]('https://m.douban.com/rexxar/api/v2/movie/' + data[0].id + '/interests?count=30&order_by=hot&anony=0&start=0&ck=&for_mobile=1', function (json) {
                     var button = json.interests.map(function(element) {
-                      return '<div class="items-line__head" style="margin-bottom: 0.4em;"><div class="items-line__title">' + element.create_time + '</div><div>评论人: ' + element.user.name + '<br />' + element.rating.value + '颗星</div></div><div class="items-line__body"><div class="full-descr"><div class="full-descr__left"><div>' + element.comment + '</div></div></div></div>';
+                      return '<div class="items-line__head" style="margin-bottom: 0.4em;"><div class="items-line__title">' + element.create_time + '</div><div>' + element.rating.value + '颗星 评论人: ' + element.user.name + '</div></div><div class="items-line__body"><div class="full-descr"><div class="full-descr__left"><div>' + element.comment + '</div></div></div></div>';
                     }).join('');
                     
 
