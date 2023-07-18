30d314254274fdb7295ae7403b2bf575641e1643
aston314 <aston.he@gmail.com>
2023-07-18 12:04:23 +0800

    Update db_review.js

--- www/build/plugins/db_review.js
+++ www/build/plugins/db_review.js
@@ -4,13 +4,28 @@
     var network = new Lampa.Reguest();
     var num;
     function douban_review(object, kpid, imdbid, num) {
+
         // Lampa.Controller.toggle('full_start');
         if (kpid != '') {
+            Lampa.Modal.open({
+                title: '',
+                html: Lampa.Template.get('modal_loading'),
+                size: 'small',
+                align: 'center',
+                mask: true,
+                onBack: function onBack() {
+                    Lampa.Modal.close();
+                    Lampa.Api.clear();
+                    Lampa.Controller.toggle('full_start');
+                }
+            });
             // $.get('https://movie.douban.com/j/subject_suggest?q=' + imdbid, function (data) {
             network["native"]('https://movie.douban.com/j/subject_suggest?q=' + imdbid, function (data) {
+                
                 if (data.length) {
                     var html = $('<div></div>');
                     network["native"]('https://m.douban.com/rexxar/api/v2/movie/' + data[0].id + '/interests?count=30&order_by=hot&anony=0&start=0&ck=&for_mobile=1', function (json) {
+                        Lampa.Modal.close(); 
                         // var button = json.interests.map(function (element) {
                         //     return '<div class="items-line__head" style="margin-bottom: 0.4em;"><div class="items-line__title">' + element.create_time + '</div><div>' + (element.rating ? element.rating.value + '颗星 ' : '') + '评论人: ' + element.user.name + '</div></div><div class="items-line__body"><div class="full-descr"><div class="full-descr__left"><div>' + element.comment + '</div></div></div></div>';
                         // }).join('');
@@ -89,6 +104,7 @@
                         }
                     });
                 } else {
+                    Lampa.Modal.close();
                     Lampa.Noty.show('没有找到影评。');
                 }
 
